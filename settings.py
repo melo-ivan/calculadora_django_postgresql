76891f0456899c5b07ebe7c219d90fa55051b319
melo-ivan <i9actions@gmail.com>
2025-07-25 21:41:57 -0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'calculadora_db',
+        'USER': 'seu_usuario_postgres',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
