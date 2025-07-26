c8b9d6d314b07b17b08ce5f9150d93099d14de3f
melo-ivan <i9actions@gmail.com>
2025-07-25 21:48:51 -0300

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -4,3 +4,14 @@ from .views import calculadora_view
 urlpatterns = [
     path('', calculadora_view, name='calculadora'),
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('core.urls')),
+]
